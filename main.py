c8d2e4279d924da63a42f4f4815f9b99cbcab9ca
egaleev <61615026+egaleev@users.noreply.github.com>
2020-08-01 23:10:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -350,7 +350,7 @@ def main():
     session = db_session.create_session()
     session.commit()
     port = int(os.environ.get("PORT", 443))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, ssl_context=context)
 
 
 if __name__ == '__main__':
